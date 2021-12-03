4af3b16fe51836edde3c9bb86d12179bdc78c37c
Shane-Deegan <93925589+Shane-Deegan@users.noreply.github.com>
2021-12-03 13:36:21 +0000

    java code
    Code for service appointment and customer

---
+++ Code/Java-code/Customer.java
@@ -0,0 +1,30 @@
+public class Customer {
+    
+    private String email;
+    private int phoneNumber;
+    private String address;
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }  
+}
---
+++ Code/Java-code/ServiceAppointment.java
@@ -0,0 +1,73 @@
+public class ServiceAppointment{
+    
+    private String date;
+    private String time;
+    private BookingType booking;
+    private PaymentRecievedType paid;
+    private boolean destroy;
+    private double cost;
+    private CarStatusType status;
+    
+    public String getDate() {
+        return date;
+    }
+    
+    public void setDate(String date) {
+        this.date = date;
+    }
+    
+    public String getTime() {
+        return time;
+    }
+    
+    public void setTime(String time) {
+        this.time = time;
+    }
+    
+    public BookingType getBooking() {
+        return booking;
+    }
+    
+    public void setBooking(BookingType booking) {
+        this.booking = booking;
+    }
+    
+    public PaymentRecievedType getPaid() {
+        return paid;
+    }
+    
+    public void setPaid(PaymentRecievedType paid) {
+        this.paid = paid;
+    }
+    
+    public boolean isDestroy() {
+        return destroy;
+    }
+    
+    public void setDestroy(boolean destroy) {
+        this.destroy = destroy;
+    }
+    
+    public double getCost() {
+        return cost;
+    }
+    
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+    
+    public CarStatusType getStatus() {
+        return status;
+    }
+    
+    public void setStatus(CarStatusType status) {
+        this.status = status;
+    }
+
+    public void cancelAppointment(){
+    }
+
+    public String toString(){
+        return "Appointment date: " + date + " Booking type: " + booking;
+    }
+}
